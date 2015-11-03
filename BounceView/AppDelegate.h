e2a23fb88b05e44b354eb7d2ebc00860e23ac593
Leexin90 <lixin2@tuandai.com>
2015-11-03 16:27:12 +0800

    Initial Commit

---
+++ BounceView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BounceView
+//
+//  Created by Leexin on 15/11/3.
+//  Copyright (c) 2015年 Leexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
